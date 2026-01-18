570d34c294f7108c063215e8466d03c15480f41c
Rahul Shrestha <rahulshrestha3132@gmail.com>
2026-01-18 15:34:41 +0545

    Refactor Navbar component and remove unused CSS styles

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,14 +2,19 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-purple-200 ">
-      <div className="logo">PassOp</div>
+    <nav className="bg-purple-200 flex items-center justify-between px-4 h-14">
+      <div className="logo font-bold">PassOp</div>
       <ul>
-        <li>
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <a href="#">Contact</a>
-          <a href="#">Download</a>
+        <li className="flex gap-4">
+          <a className="font-semibold hover:font-bold" href="#">
+            Home
+          </a>
+          <a className="font-semibold hover:font-bold" href="#">
+            About
+          </a>
+          <a className="font-semibold hover:font-bold" href="#">
+            Contact
+          </a>
         </li>
       </ul>
     </nav>

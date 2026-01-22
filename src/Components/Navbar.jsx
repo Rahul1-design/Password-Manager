92a15cb063b44162b25b020b137f4ed803d99107
Rahul Shrestha <rahulshrestha3132@gmail.com>
2026-01-22 20:17:10 +0545

    some minor styling

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-slate-900  text-white">
+    <nav className="bg-slate-900 text-2xl  text-white">
       <div className="mycontainer flex items-center justify-between px-4 py-5 h-14">
         <div className="logo font-bold">
           <span className="text-green-700">&lt;</span>

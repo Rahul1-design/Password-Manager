20a23185ef77fb3be7bdbe5369634a624c37866e
Rahul Shrestha <rahulshrestha3132@gmail.com>
2026-01-31 17:46:26 +0545

    Implement password management functionality with form handling and toggle visibility

--- src/Components/Manager.jsx
+++ src/Components/Manager.jsx
@@ -1,7 +1,16 @@
 import { useState } from "react";
 
 const Manager = () => {
-  const [click, setClick] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [form, setform] = useState({ site: "", username: "", password: "" });
+
+  const handleChange = (e) => {
+    setform({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const savePassword = () => {
+    console.log(form);
+  };
   return (
     <>
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
@@ -16,49 +25,58 @@ const Manager = () => {
         </p>
         <div className="text-white gap-4 flex flex-col p-4">
           <input
+            onChange={handleChange}
+            value={form.site}
             className="border-2 border-green-600 rounded-xl outline-0 focus:outline-green-500 focus:outline-2 bg-slate-800 text-white placeholder:text-gray-400 px-4 py-1 w-full"
             type="text"
             placeholder="Enter website URL"
-            name=""
+            name="site"
             id=""
           />
           <div className="flex justify-between gap-8 w-full ">
             <input
+              onChange={handleChange}
+              value={form.username}
               className="border-2 flex-1 border-green-600 rounded-xl outline-0 focus:outline-green-500 focus:outline-2 bg-slate-800 text-white placeholder:text-gray-400 px-4 py-1 w-full"
               type="text"
               placeholder="Enter username"
-              name=""
+              name="username"
               id=""
             />
-            <div className="relative">
+            <div className="relative flex-1">
               <input
-                className="border-2 flex-1 border-green-600 rounded-xl outline-0 focus:outline-green-500 focus:outline-2 bg-slate-800 text-white placeholder:text-gray-400 px-4 py-1 w-full"
-                type="password"
+                onChange={handleChange}
+                value={form.password}
+                className="border-2  border-green-600 rounded-xl outline-0 focus:outline-green-500 focus:outline-2 bg-slate-800 text-white placeholder:text-gray-400 px-4 py-1 w-full"
+                type={showPassword ? "text" : "password"}
                 placeholder="Enter password"
-                name=""
+                name="password"
                 id=""
               />
               <span
-                onClick={() => setClick(!click)}
+                onClick={() => setShowPassword(!showPassword)}
                 className="absolute cursor-pointer  pr-2 top-1.5 right-0"
               >
-                {click ? (
+                {showPassword ? (
                   <img
                     className="h-6 invert brightness-0 "
-                    src="/eye-password-hide.svg"
+                    src="/openeye.png"
                     alt="close eye"
                   />
                 ) : (
                   <img
                     className="h-6 invert "
-                    src="/openeye.png"
+                    src="/eye-password-hide.svg"
                     alt="open eye"
                   />
                 )}
               </span>
             </div>
           </div>
-          <button className="group bg-gray-800 rounded-xl w-1/2 h-8 flex gap-3 cursor-pointer lg:h-10 items-center justify-center hover:bg-gray-600 transition-colors duration-500 font-semibold  mx-auto text-center">
+          <button
+            onClick={savePassword}
+            className="group bg-gray-800 rounded-xl w-1/2 h-8 flex gap-3 cursor-pointer lg:h-10 items-center justify-center hover:bg-gray-600 transition-colors duration-500 font-semibold  mx-auto text-center"
+          >
             <lord-icon
               src="https://cdn.lordicon.com/lzsupfwm.json"
               trigger="hover"
